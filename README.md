b77e1b4c01d12d8d3b689fa6f75b25f45e379ea8
vaseninm <vasenin@pixelio.ru>
2013-08-15 00:50:16 +0400

    yiinitializr

---
+++ README.md
@@ -0,0 +1,3 @@
+Pixelio
+=======
+Pixelio CMS
---
+++ common/config/main.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *
+ * main.php configuration file
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @copyright 2013 2amigOS! Consultation Group LLC
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+return array(
+	'preload' => array('log'),
+	'aliases' => array(
+		'frontend' => dirname(__FILE__) . '/../..' . '/frontend',
+		'common' => dirname(__FILE__) . '/../..' . '/common',
+		'backend' => dirname(__FILE__) . '/../..' . '/backend',
+		'vendor' => dirname(__FILE__) . '/../..' . '/common/lib/vendor'
+	),
+	'import' => array(
+		'common.extensions.components.*',
+		'common.components.*',
+		'common.helpers.*',
+		'common.models.*',
+		'application.controllers.*',
+		'application.extensions.*',
+		'application.helpers.*',
+		'application.models.*'
+	),
+	'components' => array(
+		'errorHandler' => array(
+			'errorAction' => 'site/error',
+		),
+		'log' => array(
+			'class'  => 'CLogRouter',
+			'routes' => array(
+				array(
+					'class'        => 'CDbLogRoute',
+					'connectionID' => 'db',
+					'levels'       => 'error, warning',
+				),
+			),
+		),
+	),
+	'params' => array(
+		// php configuration
+		'php.defaultCharset' => 'utf-8',
+		'php.timezone'       => 'UTC',
+	)
+);
