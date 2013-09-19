05236b1f2b2050dd849fd23b39b6990ea2cc12bb
vaseninm <vasenin@pixelio.ru>
2013-09-19 22:55:27 +0400

    Флеш-директива

--- frontend/modules/PxAnimate.js
+++ frontend/modules/PxAnimate.js
@@ -6,4 +6,11 @@ angular.module('pxAnimate', [])
                 element.fancybox(config);
             }
         }
+    }).directive('pxFlash', function () {
+        return {
+            link: function ($scope, element, attrs) {
+                if (element.attr('id') === undefined) element.attr('id', Math.floor(Math.random() * (10000)) + 1000)
+                swfobject.embedSWF("myContent.swf", element.attr('id'), element.width(), element.height(), "9.0.0");
+            }
+        }
     });
