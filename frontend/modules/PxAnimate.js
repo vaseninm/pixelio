b0492ab97f098589c8c00dc09791d779f88c8971
vaseninm <vasenin@pixelio.ru>
2013-09-20 02:43:26 +0400

    vmode to wmode fix

--- frontend/modules/PxAnimate.js
+++ frontend/modules/PxAnimate.js
@@ -11,7 +11,7 @@ angular.module('pxAnimate', [])
             link: function ($scope, element, attrs) {
                 if (element.attr('id') === undefined) element.attr('id', 'flashElem_' + Math.floor(Math.random() * (10000)) + 1000);
                 swfobject.embedSWF(element.attr('px-flash'), element.attr('id'), element.attr('width'), element.attr('height'), "9.0.0", null, {}, {
-                    vmode: "transparent",
+                    wmode: "transparent",
                     menu: "false"
                 });
             }
