76e44e57adbe23dc51cd8e75129ff8de815309a3
vaseninm <vasenin@pixelio.ru>
2013-09-25 20:52:19 +0400

    Структура без поддоменов

--- frontend/controllers/PageControllers.js
+++
@@ -1,7 +0,0 @@
-function PageController($scope, $routeParams, $rootScope) {
-    $rootScope.bodyClass = 'one';
-    $rootScope.animate = 'animate-view';
-    $rootScope.setActiveMenu('page_' + $routeParams.pageCanonical);
-
-    $scope.templateUrl = 'views/page/'+$routeParams.pageCanonical+'.html';
-}
