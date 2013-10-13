3e160fb492479d787fc0eb1984b559c2dc2223e1
vaseninm <vasenin@pixelio.ru>
2013-10-13 20:29:33 +0400

    Динамический год в футере

--- angular/controllers/LayoutControllers.js
+++ angular/controllers/LayoutControllers.js
@@ -2,5 +2,7 @@ function MenuController($scope, $routeParams, $rootScope) {
     $rootScope.setActiveMenu = function(menu){
         $rootScope.selected = [];
         $rootScope.selected[menu] = true;
+
+        $rootScope.currentYear =  (new Date()).getFullYear();
     }
 }
