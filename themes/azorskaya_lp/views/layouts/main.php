35141c203ebebeddddf76714b08baa10305d9288
Gooos <guseffd@gmail.com>
2013-11-30 03:07:44 +0400

    test отступов
    Signed-off-by: Gooos <guseffd@gmail.com>

--- themes/azorskaya_lp/views/layouts/main.php
+++ themes/azorskaya_lp/views/layouts/main.php
@@ -494,6 +494,7 @@
 				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 				Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
 			</div>
+			<div class="marginBottom30"></div>
 		</div>
 		<div class="row-fluid">
 			<div class="span3 centered">
